6852c7c2435ba82b4721b89ed29fb8b93163710b
Matthias Mahler <matthias.mahler@ci4rail.com>
2023-03-07 14:51:13 +0100

    fixed linting issues

--- packages/app/src/components/Root/LogoIcon.tsx
+++ packages/app/src/components/Root/LogoIcon.tsx
@@ -1,15 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles({
-  svg: {
-    width: 'auto',
-    height: 28,
-  },
-  path: {
-    fill: '#7df3e1',
-  },
-});
 
 const LogoIcon = () => {
   return <img src="https://avatars.githubusercontent.com/u/70633499?s=35" width="35px"/>;
