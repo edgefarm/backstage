b6a05553f731058034f6eb72506fcccf9806c421
Matthias Mahler <matthias.mahler@ci4rail.com>
2023-04-04 14:16:27 +0200

    feat: added custom action

---
+++ plugins/edgefarm-backend/src/scaffolder/actions/ci4rail-fs-read.ts
@@ -0,0 +1,25 @@
+import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
+import fs from 'fs-extra';
+
+export const readFileAction = () => {
+  return createTemplateAction<{ path: string }>({
+    id: 'ci4rail:fs:read',
+    schema: {
+      input: {
+        required: ['path'],
+        type: 'object',
+        properties: {
+          path: {
+            type: 'string',
+            title: 'Filepath',
+            description: 'The filepath of the file that will be read',
+          },
+        },
+      },
+    },
+    async handler(ctx) {
+      const fileBuf = fs.readFileSync(`${ctx.workspacePath}/${ctx.input.path}`, 'utf8');
+      ctx.output('content', fileBuf.toString());
+    },
+  });
+};
