0592cb0a6c9dd473d4145ffb07109275a8c3fc06
Matthias Mahler <matthias.mahler@ci4rail.com>
2023-04-27 15:19:56 +0200

    feat: connected grafana into backstage

--- plugins/edgefarm/src/components/nodes/metrics/MetricsComponent.tsx
+++ plugins/edgefarm/src/components/nodes/metrics/MetricsComponent.tsx
@@ -1,60 +1,69 @@
-import { InfoCard, TrendLine } from '@backstage/core-components';
+import { useApi, configApiRef } from '@backstage/core-plugin-api';
+import { useEntity } from '@backstage/plugin-catalog-react';
 import { Grid } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const randData = () => {
-  return Array.from(
-    { length: 100 },
-    () => Math.floor(Math.random() * 100) / 100,
-  );
-};
+export class NodeDetails {
+  Hostname: string;
+  IPAddress: string;
 
-const randDiskData = () => {
-  const data: number[] = [];
-  while (data.length < 100) {
-    if (data.length === 0) {
-      data.push(Math.floor(Math.random() * 100) / 100);
-    }
-    const prev = data[data.length - 1];
-    let value = 0;
-    if (Math.random() > 0.7) {
-      value = prev + Math.floor(Math.random() * 100) / 100;
-    } else if (Math.random() < 0.3) {
-      value = prev - Math.floor(Math.random() * 100) / 100;
-    } else {
-      value = prev;
-    }
-
-    if (value > 1) {
-      data.push(1);
-    } else if (value < 0) {
-      data.push(0);
-    } else {
-      data.push(value);
-    }
+  constructor(payload: any) {
+    this.Hostname = payload.Hostname;
+    this.IPAddress = payload.IPAddress;
   }
-
-  return data;
-};
+}
 
 export const NodeMetricsComponent = () => {
+  const grafanaHost = 'http://localhost:3001';
+  const promPort = 9100;
+
+  const config = useApi(configApiRef);
+  const backendUrl = config.getString('backend.baseUrl');
+  const { entity } = useEntity();
+  const annotations = entity.metadata.annotations ?? {};
+  const clusterName = annotations['edgefarm.io/cluster'] ?? '';
+  const nodeName = entity.metadata.name;
+
+  const [dashboards, setDashboards] = useState<Record<string, string>>({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getMetadata = async () => {
+      const response = await fetch(
+        `${backendUrl}/api/edgefarm/${clusterName}/nodes/${nodeName}`,
+      );
+      if (response.status === 200) {
+        const payload = await response.json();
+        setDashboards({
+          'CPU Usage': `/d-solo/MstMxWs4z/node-exporter-nodes?orgId=1&refresh=30s&var-datasource=default&var-instance=${payload.IPAddress}:${promPort}&theme=light&panelId=2`,
+          'Load Average': `/d-solo/MstMxWs4z/node-exporter-nodes?orgId=1&refresh=30s&var-datasource=default&var-instance=${payload.IPAddress}:${promPort}&theme=light&panelId=3`,
+          'Memory Usage': `/d-solo/MstMxWs4z/node-exporter-nodes?orgId=1&refresh=30s&var-datasource=default&var-instance=${payload.IPAddress}:${promPort}&theme=light&panelId=4`,
+          'Memory Usage Gauge': `/d-solo/MstMxWs4z/node-exporter-nodes?orgId=1&refresh=30s&var-datasource=default&var-instance=${payload.IPAddress}:${promPort}&theme=light&panelId=5`,
+          'Disc I/O': `/d-solo/MstMxWs4z/node-exporter-nodes?orgId=1&refresh=30s&var-datasource=default&var-instance=${payload.IPAddress}:${promPort}&theme=light&panelId=6`,
+          'Disc Space Usage': `/d-solo/MstMxWs4z/node-exporter-nodes?orgId=1&refresh=30s&var-datasource=default&var-instance=${payload.IPAddress}:${promPort}&theme=light&panelId=7`,
+          'Network Received': `/d-solo/MstMxWs4z/node-exporter-nodes?orgId=1&refresh=30s&var-datasource=default&var-instance=${payload.IPAddress}:${promPort}&theme=light&panelId=8`,
+          'Network Transmitted': `/d-solo/MstMxWs4z/node-exporter-nodes?orgId=1&refresh=30s&var-datasource=default&var-instance=${payload.IPAddress}:${promPort}&theme=light&panelId=9`,
+        });
+      }
+      setIsLoading(false);
+    };
+    getMetadata();
+  }, [backendUrl, clusterName, nodeName, promPort]);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <Grid container alignItems="stretch">
-      <Grid item xs={12}>
-        <InfoCard title="Memory usage">
-          <TrendLine data={randData()} title="Trend over time" />
-        </InfoCard>
-      </Grid>
-      <Grid item xs={12}>
-        <InfoCard title="CPU usage">
-          <TrendLine data={randData()} title="Trend over time" />
-        </InfoCard>
-      </Grid>
-      <Grid item xs={12}>
-        <InfoCard title="Disk usage">
-          <TrendLine data={randDiskData()} title="Trend over time" />
-        </InfoCard>
-      </Grid>
+      {Object.entries(dashboards).map(([dashboardName, dashboardUrl]) => (
+        <Grid item md={6}>
+          <iframe
+            title={dashboardName}
+            src={`${grafanaHost}${dashboardUrl}`}
+            width="450"
+            height="200"
+            frameBorder={0}
+          />
+        </Grid>
+      ))}
     </Grid>
   );
 };
