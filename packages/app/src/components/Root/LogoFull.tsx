1e5e83bac61f3ef5498d8f2c0fa69e2bb1524a53
Armin Schlegel <armin.schlegel@gmx.de>
2023-05-04 15:48:23 +0200

    fix(logo): changed logo to edgefarm
    Signed-off-by: Armin Schlegel <armin.schlegel@gmx.de>

--- packages/app/src/components/Root/LogoFull.tsx
+++ packages/app/src/components/Root/LogoFull.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Logo from '../../theme/logos/ci4rail.png';
+import Logo from '../../theme/logos/edgefarm.png';
 
 const LogoFull = () => {
-  return <img alt="Ci4Rail Logo" src={Logo} width="180px" />;
+  return <img alt="EdgeFarm Logo" src={Logo} width="180px" />;
 };
 
 export default LogoFull;
--- packages/app/src/components/Root/LogoIcon.tsx
+++ packages/app/src/components/Root/LogoIcon.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const LogoIcon = () => {
   return (
     <img
-      alt="Ci4Rail Logo"
-      src="https://avatars.githubusercontent.com/u/70633499?s=35"
+      alt="EdgeFarm Logo"
+      src="https://avatars.githubusercontent.com/u/80546523?s=35"
       width="35px"
     />
   );
