49400ddcdaeba1a420455e8623cd3a0abe65a7d1
Matthias Mahler <matthias.mahler@ci4rail.com>
2023-05-09 08:55:58 +0200

    feat: updated grafana URLs

--- plugins/edgefarm/src/components/nodes/metrics/MetricsComponent.tsx
+++ plugins/edgefarm/src/components/nodes/metrics/MetricsComponent.tsx
@@ -1,6 +1,6 @@
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { Grid } from '@material-ui/core';
-import React, { useState } from 'react';
+import React from 'react';
 
 export const NodeMetricsComponent = () => {
   const grafanaHost = 'https://grafana.edgefarm.dev';
@@ -8,18 +8,17 @@ export const NodeMetricsComponent = () => {
   const { entity } = useEntity();
   const nodeName = entity.metadata.name;
 
-  const [dashboards, setDashboards] = useState<Record<string, string>>({});
-  setDashboards({
-    'CPU Usage': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${2}`,
-    'Load Average': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${43}`,
-    'Memory Usage': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${4}`,
-    'Memory Usage Gauge': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${5}`,
-    'Hardware Temperature': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${41}`,
-    'Disc I/O': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${6}`,
-    'Disc Space Usage': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${7}`,
-    'Network Received': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${8}`,
-    'Network Transmitted': `/d-solo/dabc6ee2-1b55-409c-8d71-4fd74acde511/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${9}`,
-  });
+  const dashboards = {
+    'CPU Usage': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${2}`,
+    'Load Average': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${43}`,
+    'Memory Usage': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${4}`,
+    'Memory Usage Gauge': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${5}`,
+    'Hardware Temperature': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${41}`,
+    'Disc I/O': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${6}`,
+    'Disc Space Usage': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${7}`,
+    'Network Received': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${8}`,
+    'Network Transmitted': `/d-solo/fb4a337e-b439-4b49-b99a-eb101cd17c55/edge-nodes?orgId=1&refresh=30s&theme=light&var-instance=${nodeName}&panelId=${9}`,
+  };
 
   return (
     <Grid container alignItems="stretch">
